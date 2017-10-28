66a2de73706c6649f4af72a4851883bcef7f85b2
Neil <neil123@gmail.com>
2017-10-27 21:24:22 -0500

    Finished up, waiting on @Siddhant-Jain to finish the reader functions to test and debug

--- FreeFlow/CSPSolving.py
+++ FreeFlow/CSPSolving.py
@@ -1,4 +1,5 @@
 # In This file write all the processing Functions
+# (row,column)
 import random
 
 
@@ -10,19 +11,60 @@ def get_next_variable_to_assign(solution_set,height,width):
         if (row, column) not in solution_set:
             yield (row, column)
 
-def can_color_be_assigned_here(color, coordinates, solve_dict)):
-    pass
+def can_color_be_assigned_here(color, coordinates, solve_dict,height,width):
+
+    return(get_four_neighbors_colors(solve_dict, color, coordinates, height, width))
+
+    #For this function, I need to check the various basic constraints. The first thing to do is to ses
+    #If the surrounding squares have either:
+    # 2 Blank, or 1 Blank and 1 Same, or no blank and 2 same
+    #So First, call get four_neighbors_colors!
 
 
 
-def smart_implementation():
-    pass
 
 
-#It's important to avoid the possibility of forming invalid "loops" of pipe.
-# One way to do this is by maintaining, for each allowed colour i of each square x, 2 bits of information:
-# whether the square x is connected by a path of definite i-coloured tiles to the first i-coloured endpoint,
-# and the same thing for the second i-coloured endpoint.
 
-# Then when recursing, don't ever pick a square that has two neighbours with the
-# same bit set (or with neither bit set) for any allowed colour.]
+
+def get_four_neighbors(coordinates,height,width):
+    neighbors=[]
+    #UP neighbor
+    if coordinates[0]>0:
+        neighbors.append((coordinates[0]-1,coordinates[1]))
+    else:
+        neighbors.append(None)
+    #Down neighbor
+    if coordinates[0]<(height-1):
+        neighbors.append((coordinates[0]+1, coordinates[1]))
+    else:
+        neighbors.append(None)
+    #Left Neighbor
+    if coordinates[1]>0:
+        neighbors.append((coordinates[0], coordinates[1]-1))
+    else:
+        neighbors.append(None)
+    #Right Neighbor
+    if coordinates[1] < (width-1):
+        neighbors.append((coordinates[0], coordinates[1]+1))
+    else:
+        neighbors.append(None)
+
+    return(neighbors)
+
+def get_four_neighbors_colors(solve_dict,color,coordinates,height,width):
+    neighbors=get_four_neighbors(coordinates,height,width)
+    ncount = 0
+    scount = 0
+
+    for sq in neighbors:
+        if sq==None:
+            ncount+=1
+        else:
+            testcolor=solve_dict[sq]
+            if testcolor==color:
+                scount+=1
+
+    if scount+ncount>=2:
+        return(True)
+    else:
+        return(False)
