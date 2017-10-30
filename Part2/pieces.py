3aa25e7b6e6fb6e7241922a0b5441575da50d106
Karthik Ravi <karthikravi@wirelessprv-10-194-167-225.near.illinois.edu>
2017-10-29 21:31:58 -0500

    Update 9:30 pm

--- Part2/pieces.py
+++ Part2/pieces.py
@@ -1,9 +1,18 @@
 class piece(object):
     def __init__(self,side,num):
+
+        #The Side of each piece
         self.side=side
+
+        #The number of each piece
         self.number=num
-        self.val=self.side+" "+str(self.number)
+
+        #The value of each piece
+        self.val=self.side+""+str(self.number)
+
+        #The space of each piece
         self.space=None
 
+    #Assigns a space on the board to a piece
     def adjust_space(self, space):
         self.space=space
